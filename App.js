4682eb71d064c630d78fdb510c8bccb1d506cfa0
mahamud babu <mahmudbabu94@gmail.com>
2021-03-30 13:22:25 -0400

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.baseText}>BlikBlok</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  baseText: 
+  {
+    color: 'white',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+
 });
+
+
+
+
+
+
+
+
+
+/*
+const textstyle = StyleSheet.create(
+  {
+    baseText: {
+      flex: 1,
+      color: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+    }
+  }
+);
+
+*/
